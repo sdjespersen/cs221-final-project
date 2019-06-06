d997eaa047b9a97e3f6760a4fb84cc441c98c49c
Scott Jespersen <sjespers@stanford.edu>
2019-06-05 22:22:03 -0700

    look at both recall and precision per-category

--- notebooks/breakdown.py
+++ notebooks/breakdown.py
@@ -9,12 +9,20 @@ def accuracy_breakdown(Ytrue, Ypred):
   """
   # Ytrue should be a pandas df
   totals = Ytrue.sum()
-  correct = collections.Counter()
+  guessed, correct = collections.Counter(), collections.Counter()
   for target, pred in zip(np.argmax(Ytrue.values, axis=1), np.argmax(Ypred, axis=1)):
+    guessed[pred] += 1
     if target == pred:
       correct[target] += 1
   correct = pd.Series({Ytrue.columns[k]: correct[k] for k in correct})
-  stats = pd.DataFrame({'totals': totals, 'correct': correct})
+  guessed = pd.Series({Ytrue.columns[k]: guessed[k] for k in guessed})
+  stats = pd.DataFrame({
+    'totals': totals,
+    'correct': correct,
+    'guessed': guessed,
+  })
   stats.correct = stats.correct.fillna(0).astype(int)
-  stats['acc'] = (correct / totals).fillna(0)
+  stats.guessed = stats.guessed.fillna(0).astype(int)
+  stats['recall'] = (correct / totals).fillna(0)
+  stats['precision'] = (correct / guessed).fillna(0)
   return stats
